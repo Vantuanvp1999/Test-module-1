d935638281f406487debe53cd344cee7f01c2fbf
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-02 11:09:30 +0700

    add

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ class MoneyBook{
     }
 }
 let moneyBooks= [
-    new MoneyBook("NVT01","DS1100","Nguyen Van A",26022022, 2016/2/13, 100000000),
-    new MoneyBook("NVT03","TK0232","Nguyen Thi A",98022011, 2019/3/3,200000000),
-    new MoneyBook("NVT02","TK0992","Le Van C", 11023304, 2022/9/12,300000000),
+    new MoneyBook("NVT01","DS1100","Nguyen Van A",26022022, 2016-2-13, 100000000),
+    new MoneyBook("NVT03","TK0232","Nguyen Thi A",98022011, 2019-3-3,200000000),
+    new MoneyBook("NVT02","TK0992","Le Van C", 11023304, 2022-9-12,300000000),
 ]
 
 
